89f03e653c94328b11e2be8e3ccdbed8962efc3a
Sanjay <106108541+sanjaywick@users.noreply.github.com>
2025-06-14 16:04:01 +0530

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="description"
+          content="Portfolio of Sanjay V V, a Blockchain Developer and Full Stack Engineer specializing in Web3 technologies."
+        />
+        <meta name="generator" content="Next.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
